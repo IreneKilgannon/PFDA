f7cc1cc23636ea5ee3c10faa45703c42570736e3
Irene Kilgannon <irene.kilgannon@gmail.com>
2024-10-27 14:57:33 +0000

    simulated 1000 rounds of risk

--- PFDA-assignments/assignment_5_risk.py
+++ PFDA-assignments/assignment_5_risk.py
@@ -1,4 +1,4 @@
-# The program should simulate 1000 individaul battle rounds in Risk (3 attackers vs 2 defender) and plot the result.
+# The program should simulate 1000 individual battle rounds in Risk (3 attackers vs 2 defender) and plot the result.
 # For extra marks: a more complicated version that simulates a full series of rounds for armies of arbitary sizes, until one side is wiped out.
 # Author: Irene Kilgannon
 
@@ -22,63 +22,62 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
+# Next task is to simulate 1000 rounds
 
-# Starting very very simple for an individual battle.
+round = 1
 
-# Create the dice
-dice_roll = np.random.randint(1, 7)
+while round <=1000 :
+    print(f'This is round: {round}')
+    # Attacker Dice Rolls
 
-# Attacker
-# Create an empty list to store the attacker dice rolls
-attacker = []
+    # Create an empty list to store the dice rolls
+    attacker = []
 
-# Roll 3 dice
+    while len(attacker) < 3:
+        # Roll 3 dice, append the result to attacker list
+        dice_roll = np.random.randint(1,7)
+        attacker.append(dice_roll)
+        attacker.sort(reverse = True)
 
-attacker = []
+    # Print attacker list
+    print(f'The attacker rolled: {attacker}')
 
-while len(attacker) < 3:
-    dice_roll = np.random.randint(1,7)
-    attacker.append(dice_roll)
-    attacker.sort(reverse = True)
+    # Defender Dice Rolls
+    defender = []
 
-# Print attacker list
-print(f'The attacker rolled: {attacker}')
+    # Roll 2 dice for the defender
+    while len(defender) < 2:
+        # Roll 2 dice, append the result to the defender list
+        dice_roll = np.random.randint(1, 7)
+        defender.append(dice_roll)
+        defender.sort(reverse = True)
 
-# remove index 2
-attacker.pop()
+    print(f'The defender rolled: {defender}')
 
-# Defender
+    # Battle - compare items in attacker and defender list
 
-defender = []
+    no_attacker_wins = 0
+    no_defender_wins = 0
 
-# Roll 2 dice for the defender
-while len(defender) < 2:
-    dice_roll = np.random.randint(1, 7)
-    defender.append(dice_roll)
-    defender.sort(reverse = True)
+    for a, d in zip(attacker, defender):
+        if  a <= d:
+            no_defender_wins += 1
+        else:
+            no_attacker_wins += 1
 
-print(f'The defender rolled: {defender}')
+    print(f'Attacker wins: {no_attacker_wins}')
+    print(f'Defender wins: {no_defender_wins}')
 
-# Battle - compare items in attacker and defender list
 
-no_attacker_wins = 0
-no_defender_wins = 0
-
-for a, d in zip(attacker, defender):
-    if  a <= d:
-        no_defender_wins = no_defender_wins + 1
+    # Winner of the battle round
+    if no_attacker_wins > no_defender_wins:
+        print(f'Attacker is the winnner of round {round}')
+    elif no_attacker_wins < no_defender_wins:
+        print(f'Defender is the winner round {round}')
     else:
-        no_attacker_wins = no_attacker_wins + 1
+        print('The battle was a draw')
 
-print(f'Attacker wins: {no_attacker_wins} ')
-print(f'Defender wins: {no_defender_wins}')
+    round += 1
 
-
-# Overall winner
-
-if no_attacker_wins > no_defender_wins:
-    print('Attacker is the winnner')
-elif no_attacker_wins < no_defender_wins:
-    print('Defender is the winner')
-else:
-    print('The battle was a draw')
+# Add in overall winner, keep running total of the no of attacker wins and no of defender wins
+# Create a plot: pie? histogram?
